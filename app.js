4989f234f942b53b929fbf959908cadfd0f1bfbb
Valeriy Kokorev <kokorev.valery@gmail.com>
2024-06-26 12:16:51 +0700

    Set

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('Hello');
+const flights = ['Russia', 'USA', 'China', 'USA', 'Russia'];
+const setFlights = new Set(flights);
+console.log(setFlights);
+console.log(setFlights.size);
+console.log(setFlights.has('Russia'));
+console.log(setFlights.has('S'));
+setFlights.add('France');
+setFlights.delete('USA');
+//setFlights.clear();
+console.log(setFlights);
+
+for (const flight of setFlights) {
+    console.log(flight);
+}
+
+console.log([...setFlights]);
